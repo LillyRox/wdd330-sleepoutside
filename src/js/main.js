55c7ad90f3ad9ef1c626950754356d1aedd94a3b
Lilly Armenta <larmenta@byupathway.com>
2025-05-14 23:30:05 -0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("/json/tents.json"); 
+const dataSource = new ProductData("tents");
 
 const element = document.querySelector(".product-list");
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,40 +1,20 @@
-// product.js
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const params = new URLSearchParams(window.location.search);
-const productId = params.get("product");
+const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-const dataSource = new ProductData("/json/tents.json"); // usa ruta relativa desde la raíz del sitio
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-function renderProductDetails(product) {
-  document.querySelector("h2").textContent = product.Brand.Name;
-  document.querySelector("h3").textContent = product.NameWithoutBrand;
-
-  const productImage = document.getElementById("productImage");
-  productImage.src = product.Image;
-  productImage.alt = product.NameWithoutBrand;
-
-  document.getElementById("productPrice").textContent = `$${product.FinalPrice}`;
-  document.getElementById("productColor").textContent = product.Colors[0].ColorName;
-  document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
-
-  document.getElementById("addToCart").dataset.id = product.Id;
-}
-
-async function showProduct() {
-  const product = await dataSource.findProductById(productId);
-  renderProductDetails(product);
-
-  document
-    .getElementById("addToCart")
-    .addEventListener("click", () => addProductToCart(product));
-}
-
-showProduct();
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
