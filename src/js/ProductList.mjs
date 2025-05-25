97c458449aedc016926ea5e6ae601089d632157c
Lilly Armenta <larmenta@byupathway.com>
2025-05-24 21:32:19 -0600

    W03 Team Activity: Dynamic Header and Footer

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,11 +1,18 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-  return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
-      <img src="${product.Image}" alt="Image of ${product.Name}">
-      <h2 class="card__brand">${product.Brand.Name}</h2>
-      <h3 class="card__name">${product.Name}</h3>
+  let discount = "";
+  if (product.FinalPrice < product.SuggestedRetailPrice) {
+    discount = `<p class="product-card__price">Discount: -$${(product.SuggestedRetailPrice - product.FinalPrice).toFixed(2)}</p> `;
+  }
+
+  return `
+  <li class="product-card">
+    <a href="/product_pages/index.html?product=${product.Id}">
+      <img src="${product.Images.PrimaryMedium}" alt="${product.Name}"/>
+      <h2 class="card__name">${product.Brand.Name}</h2>
+      <h3 class="card__brand">${product.Name}</h3>
+      ${discount}
       <p class="product-card__price">$${product.FinalPrice}</p>
     </a>
   </li>`;
@@ -19,15 +26,15 @@ export default class ProductList {
   }
 
   async init() {
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
     this.renderList(list);
   }
 
-
   renderList(list) {
-  renderListWithTemplate(productCardTemplate, this.listElement, list)
+    // const htmlStrings = list.map(productCardTemplate);
+    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+
+    // apply use new utility function instead of the commented code above
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 }
-}
-
-
-
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,7 +22,7 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-
+// get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
