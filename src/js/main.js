dfbda62ea71a7c8df0352c3b94427b5b7a32590c
Lilly Armenta <larmenta@byupathway.com>
2025-05-24 23:53:30 -0600

    main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const productList = new ProductList("tents", dataSource, element);
-productList.init();
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
