750e0d44e4f3844a88c6f2e2581a5287f386e0a6
Lilly Armenta <larmenta@byupathway.com>
2025-05-24 23:54:07 -0600

    main.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,11 @@
-import { getParam } from './utils.mjs';
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from './ProductDetails.mjs';
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
-const productId = getParam('product');
+loadHeaderFooter();
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+const dataSource = new ProductData();
+const productId = getParam("product");
+
+const productDetail = new ProductDetails(productId, dataSource);
+productDetail.init();
